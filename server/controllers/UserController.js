017027ec21c1b55aa21d809e19cf4176cd9b7c0f
yelmouss <yelmouss.devt@gmail.com>
2024-04-30 14:11:18 +0100

    add login func

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,5 +1,9 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+
 exports.register = async (req, res) => {
   try {
     const { username, password, fullname } = req.body;
@@ -11,6 +15,41 @@ exports.register = async (req, res) => {
     });
     await user.save();
     res.status(200).json({ message: "User created successfuly" });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).json({ message: "Invalid credentials" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+    const token = jwt.sign(
+      {
+        Userid: user._id,
+        isAdmin: user.isAdmin,
+        Fullname: user.fullname,
+        username: user.username,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "24h",
+      }
+    );
+    res.status(200).json({
+      message: "User logged in successfuly",
+      token,
+    });
   } catch (e) {
     console.error(e);
     res.status(500).json({
--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -3,5 +3,24 @@ const router = express.Router();
 const UserController = require("../controllers/UserController");
 
 router.post("/register", UserController.register);
+router.post("/login", UserController.login);
 
 module.exports = router;
+
+
+// La fonction router dans Express.js est une partie essentielle pour organiser 
+// et gérer les routes dans les applications web. Une route est essentiellement 
+// un chemin d'URL à travers lequel les clients peuvent accéder à différents services
+// offerts par le serveur, par exemple obtenir des données, envoyer des données, etc. 
+//  Voici comment la fonction router est utilisée et pourquoi elle est importante :
+
+// Modularité et Organisation : L'utilisation de router aide à séparer les différentes
+//  parties de l'application en modules plus petits et gérables, souvent appelés routeurs.
+//   Cela permet de maintenir une structure claire et organisée, facilitant ainsi le développement 
+//   et la maintenance du code, surtout dans de grandes applications.
+// Définition de Routes : Avec router, vous pouvez définir des routes pour différentes
+//  méthodes HTTP (comme GET, POST, PUT, DELETE, etc.). Chaque route peut être associée à une fonction
+//   spécifique qui sera exécutée lorsque la route est accédée par un client. Dans l'exemple que vous 
+//   avez donné,
+//    une route POST est définie pour l'URL "/register", et la fonction UserController.register 
+//    est définie comme gestionnaire pour cette route.
